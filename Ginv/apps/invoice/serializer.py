90ab0cf82b2f9e4daeb18293125fc255a7c3bdb9
Hasla <richkibz@gmail.com>
2024-05-08 11:04:33 +0300

    modifying string field

--- Ginv/apps/invoice/serializer.py
+++ Ginv/apps/invoice/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Invoice, Item
 
 class InvoiceSerializer(serializers.ModelSerializer):
+    client = serializers.StringRelatedField()
     class Meta:
         model = Invoice
         read_only_fields = (
@@ -9,13 +10,15 @@ class InvoiceSerializer(serializers.ModelSerializer):
             "created_at",
             "created_by",
             "modified_at",
+            "invoice_no",
             "modified_by"
         ),
         fields = (
             "id",
+            "invoice_no",
             "client",
             "client_name",
-            "client_email"
+            "client_email",
             "client_org_id",
             "client_add1",
             "client_add2",
@@ -31,10 +34,22 @@ class InvoiceSerializer(serializers.ModelSerializer):
             "gross_amount",
             "vat_amount",
             "net_amount",
-            "discount_amount"
-
-
-
-
+            "discount_amount",
+            "is_paid"
         )
 
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        read_only_fields = (
+            "invoice",
+        )
+        fields = (
+            "id",
+            "title",
+            "quantity",
+            "unit_price",
+            "net_amount",
+            "vat_rate",
+            "discount"
+        )
