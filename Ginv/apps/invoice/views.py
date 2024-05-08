977f62c04a153721bab1ad6d33d2f7294b2b395e
Hasla <richkibz@gmail.com>
2024-05-08 11:05:09 +0300

    adding items viewset

--- Ginv/apps/invoice/views.py
+++ Ginv/apps/invoice/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from .models import Invoice, Item
+from .serializer import InvoiceSerializer, ItemSerializer
 
-# Create your views here.
+from rest_framework import viewsets
+
+from django.core.exceptions import PermissionDenied
+
+
+class InvoiceViewset(viewsets.ModelViewSet):
+    serializer_class = InvoiceSerializer
+    queryset = Invoice.objects.all()
+
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
+    
+    def perform_create(self, serializer):
+        team = self.request.user.team.first()
+        serializer.save(created_by=self.request.user, team=team)
+
+    def perform_update(self, serializer):
+        obj = self.get_object()
+
+        if self.request.user != obj.created_by:
+            raise PermissionDenied("Wrong object user")
+        
+        serializer.save()
+
+
+class ItemViewset(viewsets.ModelViewSet):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+    def get_queryset(self):
+        invoice_id = self.request.GET.get('invoice_id', 0)
+
+        return self.queryset.filter(invoice_id=invoice_id)
