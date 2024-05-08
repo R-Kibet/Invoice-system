48275649c234fc2d8a077a966e91a29f7e3d4df1
Hasla <richkibz@gmail.com>
2024-05-08 11:04:53 +0300

    adding items urls path

---
+++ Ginv/apps/invoice/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import InvoiceViewset, ItemViewset
+
+router = DefaultRouter()
+router.register("invoices", InvoiceViewset, basename="invoices")
+router.register("items", ItemViewset, basename="items")
+
+urlpatterns = [
+    path("", include(router.urls))
+]
